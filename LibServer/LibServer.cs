7a3c4f59ba1d2607c5fbfe7139b2f9aa8208f587
Kelvin Wijkniet <31469915+KWijkniet@users.noreply.github.com>
2021-10-28 16:03:42 +0200

    Initial commit

---
+++ LibServer/LibServer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using LibData;
+
+
+namespace LibServer
+{
+    // Note: Do not change this class.
+    public class Setting
+    {
+        public int ServerPortNumber { get; set; }
+        public int BookHelperPortNumber { get; set; }
+        public int UserHelperPortNumber { get; set; }
+        public string ServerIPAddress { get; set; }
+        public string BookHelperIPAddress { get; set; }
+        public string UserHelperIPAddress { get; set; }
+        public int ServerListeningQueue { get; set; }
+    }
+
+
+    // Note: Complete the implementation of this class. You can adjust the structure of this class. 
+    public class SequentialServer
+    {
+        // all the required settings are provided in this file
+        //public string configFile = @"../ClientServerConfig.json";
+        public string configFile = @"../../../../ClientServerConfig.json"; // for debugging
+
+        private Setting settings;
+        private IPAddress ipAddress;
+
+        public SequentialServer()
+        {
+            //todo: implement the body. Add extra fields and methods to the class if it is needed
+
+            // read JSON directly from a file
+            try
+            {
+                string configContent = File.ReadAllText(configFile);
+                this.settings = JsonSerializer.Deserialize<Setting>(configContent);
+                this.ipAddress = IPAddress.Parse(settings.ServerIPAddress);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[Client Exception] {0}", e.Message);
+            }
+        }
+
+        public void start()
+        {
+            //todo: implement the body. Add extra fields and methods to the class if it is needed
+
+        }
+    }
+
+}
+
+
+
